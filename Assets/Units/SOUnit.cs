ab7c56329db35d7ccace5859a2f84112a9d118d2
Queejon <queejonjin@gmail.com>
2023-01-30 14:32:00 -0500

    Added Units

---
+++ Assets/Units/SOUnit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WorldTree.Core
+{
+    [CreateAssetMenu(fileName = "Unit", menuName = "WorldTree/Unit", order = 0)]
+    public class SOUnit : ScriptableObject
+    {
+        public string unitName;
+        public int cost;
+        public float moveSpeed;
+        
+        public float attackPower;
+        public float attackSpeed;
+        
+        public float health;
+    }
+}
